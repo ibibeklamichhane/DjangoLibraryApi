0dfbb97a81d2e215f909a0498eb33a9b12b907a9
ibibeklamichhane <bibeklc714@gmail.com>
2024-01-31 22:39:18 +0545

    Updated urls

---
+++ library_management/library/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    CreateUserView, ListAllUsersView, GetUserByIDView,
+    AddNewBookView, ListAllBooksView, GetBookByIDView, AssignUpdateBookDetailsView,
+    BorrowBookView, ReturnBookView, ListAllBorrowedBooksView,
+)
+
+urlpatterns = [
+    path('users/create/', CreateUserView.as_view(), name='create-user'),
+    path('users/', ListAllUsersView.as_view(), name='list-all-users'),
+    path('users/<int:UserID>/', GetUserByIDView.as_view(), name='get-user-by-id'),
+
+    path('books/create/', AddNewBookView.as_view(), name='add-new-book'),
+    path('books/', ListAllBooksView.as_view(), name='list-all-books'),
+    path('books/<int:BookID>/', GetBookByIDView.as_view(), name='get-book-by-id'),
+    path('books/details/<int:BookID>/', AssignUpdateBookDetailsView.as_view(), name='assign-update-book-details'),
+
+    path('borrowed_books/borrow/', BorrowBookView.as_view(), name='borrow-book'),
+    path('borrowed_books/return/', ReturnBookView.as_view(), name='return-book'),
+    path('borrowed_books/', ListAllBorrowedBooksView.as_view(), name='list-all-borrowed-books'),
+]
